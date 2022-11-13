5f51f3064c5e1e2fec854ec1d2cd780f5ecc8c73
caesar <x246libra@hotmail.com>
2022-11-13 13:23:08 +0800

    binary search: left bound, right bound

--- linear/binary_search.py
+++ linear/binary_search.py
@@ -1,25 +1,100 @@
 def binary_search(data: list[int], target: int) -> int:
+    # 因為把 right 定義為 len-1
+    # 可得搜尋區間 = [left, right]
+    #
+    # 若把 right 定義為 len
+    # 搜尋區間 = [left, right) 避免索引越界
     left = 0
     right = len(data)-1
 
-    # 背下來
-    # 寫成 while left <= right 的話
-    # 當 target 不存在的時候, 會進入無窮迴圈
-    #
-    # 目標存在的話, left == right, 就可以得到答案, 不會進入 while 判斷區塊
-    # 所以若是有等於的情況出現在 while 判斷區塊, 可以理解為 目標不存在
-    while left < right:
+    while left <= right:
         mid = left+(right-left)//2
-        print(left, right, mid)
+        # print('before', left, right, mid)
 
-        if data[mid] > target:
-            right = mid
-        elif data[mid] < target:
-            left = mid+1
-        else:
+        current = data[mid]
+        if current == target:
             return mid
+        elif current < target:
+            left = mid+1
+        elif current > target:
+            right = mid-1
+
+        # print('after', left, right, mid)
 
     return -1
+
+
+def left_bound(data: list[int], target: int) -> int:
+    left = 0
+    right = len(data)-1
+
+    while left <= right:
+        mid = left+(right-left)//2
+        # print(left, right, mid)
+
+        current = data[mid]
+        if current == target:
+            right = mid-1
+        elif current < target:
+            left = mid+1
+        elif current > target:
+            right = mid-1
+
+    return -1 if left >= len(data) or data[left] != target else left
+
+
+def right_bound_fail(data: list[int], target: int) -> int:
+    left = 0
+    right = len(data)-1
+
+    while left < right:
+        mid = left+(right-left)//2
+        # print(left, right, mid)
+
+        current = data[mid]
+        if current == target:
+            # 計算 mid, 除以2的特性, 得到的數值會往小數值靠近
+            # left=0, right=1 -> mid=0
+            # >> if not plus 0: left=mid=0, left 維持不動
+            # 變成無窮迴圈
+            #
+            # left 要往右邊靠近
+            # 需要另外 + 1, 但若進行 +1 運算
+            # 要考慮分別考慮不同情況
+            #
+            # 1. 正常的前進一步
+            # left=0, right=1 -> mid=0
+            # left=mid+1=1
+            #
+            # 2. 多走一步
+            # left=1, right=3 -> mid=2
+            # left=mid+1=3
+            left = mid+1
+        elif current < target:
+            left = mid+1
+        elif current > target:
+            right = mid-1
+
+    return right-1 if data[right-1] == target else -1
+
+
+def right_bound(data: list[int], target: int) -> int:
+    left = 0
+    right = len(data)-1
+
+    while left <= right:
+        mid = left+(right-left)//2
+        # print(left, right, mid)
+
+        current = data[mid]
+        if current == target:
+            left = mid+1
+        elif current < target:
+            left = mid+1
+        elif current > target:
+            right = mid-1
+
+    return -1 if right < 0 or data[right] != target else right
 
 # 所有元素的數值 必須是唯一的, 不可重複
 # search 和 sort 是密不可分的
@@ -33,7 +108,21 @@ def binary_search(data: list[int], target: int) -> int:
 
 
 if __name__ == '__main__':
-    # print('ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 50))
-    # print('ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 40))
-    # print('ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 1))
-    print('ans =', binary_search([1, 4, 5, 9, 20, 40, 60], -1))
+    # print('binary_search ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 1))
+    # print('binary_search ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 40))
+    # print('binary_search ans =', binary_search([1, 4, 5, 9, 20, 40, 60], 50))
+    # print('binary_search ans =', binary_search([1, 4, 5, 9, 20, 40, 60], -1))
+    print()
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], 1))
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], 6))
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], 7))
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], 4))
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], -2))
+    print('left_bound ans =', left_bound([1, 3, 3, 3, 3, 6], 3))
+    print()
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], 1))
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], 6))
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], 7))
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], 4))
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], -2))
+    print('right_bound ans =', right_bound([1, 3, 3, 3, 3, 6], 3))
