3900360789769c0679e3c066acd295761ec08d2c
caesar <x246libra@hotmail.com>
2022-11-20 21:34:13 +0800

    house_linear_memo, p207

---
+++ dp/house.py
@@ -0,0 +1,28 @@
+from tool import debugHelper
+
+
+def house_linear_memo(nums: list[int]) -> int:
+    # 算法筆記 p207
+
+    memo: dict[int, int] = dict()
+
+    @debugHelper
+    def dp(nums: list[int], start: int) -> int:
+        nonlocal memo
+        N = len(nums)
+
+        if start >= N:
+            return 0
+
+        if start in memo:
+            return memo[start]
+
+        memo[start] = max(dp(nums, start+1), dp(nums, start+2)+nums[start])
+        return memo[start]
+
+    return dp(nums, 0)
+
+
+if __name__ == '__main__':
+    sol1 = house_linear_memo([2, 1, 7, 9, 3, 1])
+    print(f'{house_linear_memo.__name__} expect = 12, actual = {sol1}')
