419afce21bcca80cffef54f595166aa6969127e4
caesar <x246libra@hotmail.com>
2022-11-21 21:50:41 +0800

    refactor sort project layout

--- sort/py_std.py
+++ sort/py_std.py
@@ -4,7 +4,8 @@
 # multi condition -> lamdba return tuple
 # https://blog.csdn.net/y12345678904/article/details/77507552
 
-data = [(1, 2), (7, 1), (3, 6), (4, 5), (2, 9)]
+if __name__ == '__main__':
+    data = [(1, 2), (7, 1), (3, 6), (4, 5), (2, 9)]
 
-data.sort(key=lambda k: (k[0]*k[1], -k[0]))
-print(data)
+    data.sort(key=lambda k: (k[0]*k[1], -k[0]))
+    print(data)
