e7d33e82be2011f7df2c498444b7a176fbcb4c38
caesar <x246libra@hotmail.com>
2022-11-21 14:31:15 +0800

    lowest common ancestor, leetcode 236

---
+++ tree/lca.py
@@ -0,0 +1,48 @@
+import factory
+import traversal
+from model import TreeNode
+
+
+def lca(root, p, q) -> TreeNode:
+    # 算法筆記 p270
+    # lowest common ancestor
+
+    if root == None:
+        return None
+
+    # 此 base case 要再想想來由
+    # 題目說明 p, q 一定存在
+    if root == q or root == p:
+        # print(
+        #     f'root={(p.value, id(root))}, p={(p.value, id(p))}, q={(q.value, id(q))}')
+        return root
+
+    left = lca(root.left, p, q)
+    right = lca(root.right, p, q)
+
+    if left == right == None:
+        return None
+    elif left != None and right != None:
+        return root
+    elif left != None and right == None:
+        return left
+    elif left == None and right != None:
+        return right
+
+
+if __name__ == '__main__':
+    root1 = factory.level_order(
+        [3, 5, 1, 6, 2, 0, 8, None, None, 7, 4, None, None, None, None, None, None, None, None])
+    print(traversal.in_order_recursive(root1))
+
+    p = root1.left.left
+    q = root1.left.right.left
+    print(f'p={(p.value, id(p))}, q={(q.value, id(q))}')
+    print(f'lca node = {lca(root1,p,q).value}\n')
+
+    # 如果 p, q 不存在 tree 中
+    # 此解法會有錯誤
+    # p = root1.left.left
+    # q = TreeNode(9)
+    # print(f'p={(p.value, id(p))}, q={(q.value, id(q))}')
+    # print(f'lca node = {lca(root1,p,q)}\n')
