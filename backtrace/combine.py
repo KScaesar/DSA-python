b4a5c29593485b0a45b6a783f19f20f59d5c7cc9
caesar <x246libra@hotmail.com>
2022-11-22 11:48:45 +0800

    backtrace combination

---
+++ backtrace/combine.py
@@ -0,0 +1,29 @@
+from backtrace.tool import debugHelper
+
+
+def combine(n: int, k: int) -> list[list[int]]:
+    result: list[list[int]] = []
+
+    @debugHelper
+    def backtrace(n: int, k: int, start: int, track: list[int]):
+        nonlocal result
+
+        if len(track) == k:
+            result.append(track.copy())
+            return
+
+        for i in range(start, n + 1):
+            track.append(i)
+            backtrace(n, k, i + 1, track)  # 注意 不要寫成 backtrace(n, k, start + 1, track)
+            track.pop()
+
+    backtrace(n, k, 1, [])
+    return result
+
+
+def main():
+    print(combine(4, 2))
+
+
+if __name__ == '__main__':
+    main()
