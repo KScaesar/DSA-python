e5ab32d866f1bb9c492f7a426f5d15808f962933
caesar <x246libra@hotmail.com>
2022-12-01 11:05:23 +0800

    docs: binary search

--- linear/binary_search.py
+++ linear/binary_search.py
@@ -5,50 +5,59 @@ def binary_search(data: list[int], target: int) -> int:
     # 若把 right 定義為 len
     # 搜尋區間 = [left, right) 避免索引越界
     left = 0
-    right = len(data)-1
+    right = len(data) - 1
 
     while left <= right:
-        mid = left+(right-left)//2
+        mid = left + (right - left) // 2
         # print('before', left, right, mid)
 
         current = data[mid]
         if current == target:
             return mid
         elif current < target:
-            left = mid+1
+            left = mid + 1
         elif current > target:
-            right = mid-1
+            right = mid - 1
 
         # print('after', left, right, mid)
 
     return -1
 
 
+# https://labuladong.github.io/algo/2/20/30/
+#
+# 当目标元素 target 不存在数组 nums 中时，搜索左侧边界的二分搜索的返回值可以做以下几种解读
+#
+# 1、返回的这个值是 nums 中大于等于 target 的最小元素索引。
+# 2、返回的这个值是 target 应该插入在 nums 中的索引位置。
+# 3、返回的这个值是 nums 中小于 target 的元素个数。
+#
+# 比如在有序数组 nums = [2,3,5,7] 中搜索 target = 4，搜索左边界的二分算法会返回 2，你带入上面的说法，都是对的。
 def left_bound(data: list[int], target: int) -> int:
     left = 0
-    right = len(data)-1
+    right = len(data) - 1
 
     while left <= right:
-        mid = left+(right-left)//2
+        mid = left + (right - left) // 2
         # print(left, right, mid)
 
         current = data[mid]
         if current == target:
-            right = mid-1
+            right = mid - 1
         elif current < target:
-            left = mid+1
+            left = mid + 1
         elif current > target:
-            right = mid-1
+            right = mid - 1
 
     return -1 if left >= len(data) or data[left] != target else left
 
 
 def right_bound_fail(data: list[int], target: int) -> int:
     left = 0
-    right = len(data)-1
+    right = len(data) - 1
 
     while left < right:
-        mid = left+(right-left)//2
+        mid = left + (right - left) // 2
         # print(left, right, mid)
 
         current = data[mid]
@@ -69,32 +78,33 @@ def right_bound_fail(data: list[int], target: int) -> int:
             # 2. 多走一步
             # left=1, right=3 -> mid=2
             # left=mid+1=3
-            left = mid+1
+            left = mid + 1
         elif current < target:
-            left = mid+1
+            left = mid + 1
         elif current > target:
-            right = mid-1
+            right = mid - 1
 
-    return right-1 if data[right-1] == target else -1
+    return right - 1 if data[right - 1] == target else -1
 
 
 def right_bound(data: list[int], target: int) -> int:
     left = 0
-    right = len(data)-1
+    right = len(data) - 1
 
     while left <= right:
-        mid = left+(right-left)//2
+        mid = left + (right - left) // 2
         # print(left, right, mid)
 
         current = data[mid]
         if current == target:
-            left = mid+1
+            left = mid + 1
         elif current < target:
-            left = mid+1
+            left = mid + 1
         elif current > target:
-            right = mid-1
+            right = mid - 1
 
     return -1 if right < 0 or data[right] != target else right
+
 
 # 所有元素的數值 必須是唯一的, 不可重複
 # search 和 sort 是密不可分的
