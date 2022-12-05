11720e004cb9fb0116e795df64d7f44d3bf1eea1
caesar <x246libra@hotmail.com>
2022-12-05 14:10:34 +0800

    0215_Kth_Largest_Element_in_an_Array

---
+++ top_k/0215_Kth_Largest_Element_in_an_Array.py
@@ -0,0 +1,87 @@
+import heapq
+
+
+class Solution:
+    # https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+    # https://leetcode.com/problems/kth-largest-element-in-an-array/solutions/60294/solution-explained/?orderBy=most_votes
+    def findKthLargest(self, nums: list[int], k: int) -> int:
+        # 成功
+        # 但以 heap 來說
+        # 不是最佳解, 請看 v2
+        # 此解法 時間 空間 都比 單純 sort 還爛
+
+        max_heap = [-x for x in nums]
+        heapq.heapify(max_heap)
+
+        ans = None
+        for _ in range(k):
+            ans = -heapq.heappop(max_heap)
+
+        return ans
+
+    def findKthLargest_v2(self, nums: list[int], k: int) -> int:
+        # https://blog.techbridge.cc/2020/04/12/leetcode-%E5%88%B7%E9%A1%8C-pattern-top-k-elements/
+        #
+        # O(nlogk)
+        # 如果 n 比 k 大很多很多，隨時只需要維持 heap 裡面的排序就好
+        # 因為 heap 只有 k 個 element，所以維持這個排序所需要的 O(logk) 時間相對就很少
+
+        # 錯誤1: heap 底層的 list 必須為 空陣列, 不然 heap 插入會有錯誤
+        # max_heap = [0] * k
+
+        # 錯誤2: 求第 k 大的數目, 應該使用 min_heap, 反之用 max_heap
+        # max_heap = []
+
+        min_heap = []
+        for num in nums:
+            heapq.heappush(min_heap, num)
+            print(min_heap)
+            if len(min_heap) > k:
+                heapq.heappop(min_heap)
+        return min_heap[0]
+
+    def findKthLargest_v3(self, nums: list[int], k: int) -> int:
+        # 如果想要更好的效率, 利用 快速排序 partition 概念
+        # https://leetcode.com/problems/kth-largest-element-in-an-array/solutions/60294/Solution-explained/
+
+        target = k - 1
+        left = 0
+        right = len(nums) - 1
+
+        while True:
+            position = self.partition_v1(nums, left, right)
+            if position == target:
+                return nums[target]
+            elif position > target:
+                right = position - 1
+            elif position < target:
+                left = position + 1
+
+    def partition_v1(self, nums, left, right) -> int:
+        pivot = nums[left]
+
+        # 仔細想想條件 是 < 還是 <=
+        # 可以想成 要保留最後一個位置進行 nums[left]=pivot
+        # 所以相等的時候 不進行動作
+        while left < right:
+            while left < right and pivot >= nums[right]:  # 遞減數列用 pivot >= nums[right]
+                right -= 1
+            nums[left] = nums[right]
+
+            while left < right and pivot < nums[left]:
+                left += 1
+            nums[right] = nums[left]
+
+        nums[left] = pivot
+        return left
+
+    def partition_v2(self, nums, left, right) -> int:
+        pivot = nums[left]
+        return left
+
+
+if __name__ == '__main__':
+    obj = Solution()
+
+    sol1 = obj.findKthLargest_v3([3, 2, 3, 1, 2, 4, 5, 5, 6], 4)
+    print(f'expect=4 actual={sol1}')
