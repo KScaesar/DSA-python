5c4d0044fbed9d1d32a13c5a117da13d2b1a8d71
caesar <x246libra@hotmail.com>
2022-12-22 11:01:45 +0800

    0048. Rotate Image

---
+++ leetcode/0048. Rotate Image.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+class Solution:
+    # https://leetcode.com/problems/rotate-image/
+
+    # 算法筆記 p117
+    # 如何走訪矩陣 上三角 下三角
+
+    # 如何走訪斜線 Zigzag (or diagonal) traversal
+    # https://www.bilibili.com/video/BV1wK4y1j7JU/
+    # https://www.geeksforgeeks.org/zigzag-or-diagonal-traversal-of-matrix/
+
+    def rotate(self, matrix: List[List[int]]) -> None:
+        # 利用矩陣以對角線為中心, 互換的特性
+        # https://labuladong.github.io/algo/2/20/26/
+
+        # 時間複雜度分析
+        # 以及另一種解法
+        # 每次遞迴交換 四個 cell
+        # https://leetcode.com/problems/rotate-image/solutions/1037232/rotate-image/?orderBy=most_votes
+
+        size = len(matrix)
+
+        for row in range(size):
+            for col in range(row + 1, size):
+                matrix[row][col], matrix[col][row] = matrix[col][row], matrix[row][col]
+
+        for row in range(size):
+            left = 0
+            right = size - 1
+            while left < right:
+                matrix[row][left], matrix[row][right] = matrix[row][right], matrix[row][left]
+                left += 1
+                right -= 1
+
+
+if __name__ == '__main__':
+    data1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(Solution().rotate(data1))
+    print(data1)
