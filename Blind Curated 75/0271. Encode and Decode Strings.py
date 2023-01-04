3067cb0855059c6a4bd0ce80674f90a42c5f5e16
caesar <x246libra@hotmail.com>
2023-01-04 19:27:48 +0800

    0271. Encode and Decode Strings

---
+++ Blind Curated 75/0271. Encode and Decode Strings.py
@@ -0,0 +1,14 @@
+class Solution:
+    # https://www.lintcode.com/problem/659/
+
+    # 感覺這題目 在考 系統設計
+    # 暫時跳過 只看別人的寫法
+    def encode(self, strs):
+        pass
+
+    def decode(self, str):
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution())
