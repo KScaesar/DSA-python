0deb92c6fccd4a30b577e2af2c9c9a50846ea3b4
caesar <x246libra@hotmail.com>
2023-03-31 12:10:36 +0800

    review: 0049

--- Blind Curated 75/0049. Group Anagrams.py
+++ Blind Curated 75/0049. Group Anagrams.py
@@ -6,6 +6,7 @@ class Solution:
 
     # 沒難度 複習可跳過
     def groupAnagrams_v1(self, strs: List[str]) -> List[List[str]]:
+        # O( N * K*logK )
         groups = dict()
 
         for word in strs:
@@ -18,6 +19,36 @@ class Solution:
 
         return list(groups.values())
 
+    def groupAnagrams_v2(self, strs: List[str]) -> List[List[str]]:
+        # https://hello-kirby.hashnode.dev/leetcode-48group-anagrams
+
+        # O( N * K )
+
+        memo = dict()
+        ord_a = ord('a')
+        for idx, word in enumerate(strs):
+            count = [0 for _ in range(26)]
+            for char in word:
+                count[ord(char) - ord_a] += 1
+
+            # 次數超過 10, 此方法無效,
+            # 比如 某個字母 次數 10
+            # 會搞錯以為
+            # 是 兩個字母 分別是次數 1 跟 0
+            # key = "".join([str(v) for v in count])
+
+            key = "".join([str(f'{chr(ord_a + i)}{v}') for i, v in enumerate(count)])
+            print(f'{idx} {word} {count} {key}')
+
+            if not memo.get(key):
+                memo[key] = [word]
+            else:
+                memo[key].append(word)
+
+        return [group for group in memo.values()]
+
 
 if __name__ == '__main__':
-    print(Solution().groupAnagrams_v1(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    # print(Solution().groupAnagrams_v1(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    # print(Solution().groupAnagrams_v2(["eat", "tea", "tan", "ate", "nat", "bat"]))
+    print(Solution().groupAnagrams_v2(["bdddddddddd", "bbbbbbbbbbc"]))
